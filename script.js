8361d2a5b5fc73483dbab3bde46fe8387c07af39
akopponen <aleksi05kopponen@gmail.com>
2023-02-09 16:25:10 +0200

    source

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
 }
+// https://youtu.be/4ykAepVkG5Y?t=4964 | Kohta 1:22:44
