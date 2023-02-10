9609536dddcbabdb7ab3a75654aaec873bf59e2e
akopponen <aleksi05kopponen@gmail.com>
2023-02-10 15:24:21 +0200

    main-bar_changes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
+let profileMenu = document.getElementById("profileMenu");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
+}
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
 }
 // https://youtu.be/4ykAepVkG5Y?t=4964 | Kohta 1:22:44
